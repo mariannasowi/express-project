a692a01d3530b2c5da2f1ef6cdfd1b4ec0b132b1
Marianna Sowinska <marianna.sowi@gmail.com>
2020-12-08 15:27:58 +0100

    Task module 27.2

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.listen(9000, () => {
+    console.log('Server is running on port: 9000');
+  });
+
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+
+app.use('/user/', (req, res, next) => {
+    res.show('forbidden.html');
+  });
+
+app.get('/', (req, res) => {
+  res.show('home.html');
+});
+
+app.get('/about', (req, res) => {
+  res.show('about.html');
+});
+
+app.get('/home', (req, res) => {
+  res.show('home.html');
+});
+
+app.get('/info', (req, res) => {
+  res.show('404.html');
+});
+
+app.use((req, res) => {
+  res.status(404).show('test.png');
+});
